d5888422cd46fde6312936f756674e64bf2031e2
walidelzo <walidezo@gmail.com>
2021-12-07 16:13:45 +0200

    add new file StaticConstants

---
+++ src/com/common/StaticConstants.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c)   walid elzohery  ٧‏/١٢‏/٢٠٢١ ٤:١١ م
+ * All Rights reserved
+ */
+
+package com.common;
+
+public class StaticConstants {
+    final static int SEARCH_BY_ID=1;
+    final static int SEARCH_BY_NAME=2;
+    final static int SEARCH_BY_CATEGORY=3;
+}
